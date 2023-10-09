41df445d5ea532d96e8d71ea6350e15853eff33e
Nicolas <nicolusmaluleke@gmail.com>
2023-10-09 21:50:37 +0200

    Removed unused code

--- ReactCore/Controllers/CounterController.cs
+++ ReactCore/Controllers/CounterController.cs
@@ -7,12 +7,11 @@ namespace ReactCore.Controllers
     [Route("[controller]")]
     public class CounterController : ControllerBase
     {
-        private int _counter = 0;
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Inertia.Render("Counter", new { Count = _counter });
+            return Inertia.Render("Counter");
         }
     }
 }
